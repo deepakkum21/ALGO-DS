cabd85fb2e9bddd0de36b6223ee84f3874db138f
Deepk kumar <deepakanjani21@gmail.com>
2022-04-08 06:53:44 +0530

    Kruskal

---
+++ Using Java/Graph/MST/UndirectedEdge.java
@@ -0,0 +1,11 @@
+public class UndirectedEdge {
+    public int firstIndex;
+    public int secondIndex;
+    public int weight;
+
+    UndirectedEdge(int firstIndex, int secondIndex, int weight) {
+        this.firstIndex = firstIndex;
+        this.secondIndex = secondIndex;
+        this.weight = weight;
+    }
+}
