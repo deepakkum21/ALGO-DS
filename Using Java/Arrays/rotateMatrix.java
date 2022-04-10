1b0b9341abaa7815a4b6d954b14cb32a05f350c0
Deepk kumar <deepakanjani21@gmail.com>
2022-04-10 19:15:14 +0530

    2nd approach added

--- Using Java/Arrays/rotateMatrix.java
+++ Using Java/Arrays/rotateMatrix.java
@@ -1,26 +1,29 @@
 import java.util.Arrays;
+
 class Main {
   public static void main(String[] args) {
     Main mn = new Main();
-    int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
+    int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
     mn.rotateMatrix(matrix);
     System.out.println(Arrays.deepToString(matrix));
 
-
   }
 
+  // 1. first transpose the matrix and then reverse the each row data
+  // 2. down option
   public boolean rotateMatrix(int[][] matrix) {
-    if (matrix.length == 0 || matrix.length != matrix[0].length) return false;
+    if (matrix.length == 0 || matrix.length != matrix[0].length)
+      return false;
     int n = matrix.length;
-    for (int layer = 0; layer < n/2; layer++) {
+    for (int layer = 0; layer < n / 2; layer++) {
       int first = layer;
       int last = n - 1 - layer;
-      for (int i=first; i<last; i++) {
+      for (int i = first; i < last; i++) {
         int offset = i - first;
         int top = matrix[first][i];
-        matrix[first][i] = matrix[last-offset][first];
-        matrix[last-offset][first] = matrix[last][last-offset];
-        matrix[last][last-offset] = matrix[i][last];
+        matrix[first][i] = matrix[last - offset][first];
+        matrix[last - offset][first] = matrix[last][last - offset];
+        matrix[last][last - offset] = matrix[i][last];
         matrix[i][last] = top;
 
       }
@@ -29,6 +32,5 @@ class Main {
     return true;
 
   }
-
 
 }
