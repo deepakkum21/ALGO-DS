877b829ce6d2fdea164abc58fc85595514abdc8d
Deepk kumar <deepakanjani21@gmail.com>
2022-04-11 22:28:44 +0530

    1st -ve no

---
+++ SLIDING_WINDOW/first-negative-no-subset-array.java
@@ -0,0 +1,5 @@
+// maintain a QUEUE for -ve no
+// add -ve no in queue
+// pop out the -ve no for each window from starting as that will be the 1st -ve
+// no for each window,
+// if no -ve no present in queue, then show 0
