7ffc91a0667a4cc44248a241e797a8985d2debd1
Deepk kumar <deepakanjani21@gmail.com>
2022-04-11 22:34:47 +0530

    count of angram

---
+++ SLIDING_WINDOW/count-occurences-of-anagram.java
@@ -0,0 +1,15 @@
+// maintain the object with count of letter for anagram word
+// eg deepak word \
+// {
+// 'd':1,
+// 'e':2,
+// 'p':1,
+// 'a':1,
+// 'k':1,
+// }
+
+// for each letter in window decrement the letter count for each letter found in
+// the window
+// if count of each letter is 0 then anagram found
+// if letter is removed from window do -1 in angarm object and if add then
+// increment in angram object
